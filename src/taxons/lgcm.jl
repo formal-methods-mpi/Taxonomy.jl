4858a47596338dc3bab8581c9106ecf2c6c0e5a5
nickhaf <hafiznij@hu-berlin.de>
2022-08-01 13:45:57 +0200

    added type for default ntimepoints setting

--- src/taxons/lgcm.jl
+++ src/taxons/lgcm.jl
@@ -1,12 +1,15 @@
 struct LGCM <: Taxon
-    timecoding::Judgement 
+    timecoding::Judgement
     ntimepoints::Judgement
     npredictors::Judgement
     nonlinearfunction::Judgement
 end
 
+function length_time(timecoding::Vector{<:Number}) length(timecoding) end
+length_time([1,2])
+
 function LGCM(; timecoding, 
-    ntimepoints = length(timecoding),
+    ntimepoints = length_time(timecoding),
     npredictors = 0,
     nonlinearfunction = 0)
     LGCM(timecoding, 
