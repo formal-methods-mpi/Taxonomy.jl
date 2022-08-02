fc24728d7cbed814f0dbd093da38e3eedcf01273
Aaron Peikert <aaron.peikert@posteo.de>
2022-08-02 17:22:26 +0200

    check earlier for missingness

--- src/taxons/lgcm.jl
+++ src/taxons/lgcm.jl
@@ -10,14 +10,16 @@ function LGCM(;
     ntimepoints = NoJudgement(),
     npredictors = 0,
     nonlinearfunction = 0)
-
-    implied_ntimepoints = length(rating(timecoding))
-    if ismissing(rating(ntimepoints))
-        ntimepoints = implied_ntimepoints
+    if !ismissing(rating(timecoding))
+        implied_ntimepoints = length(rating(timecoding))
+        if ismissing(rating(ntimepoints))
+            ntimepoints = implied_ntimepoints
+        end
+        if ntimepoints != implied_ntimepoints
+            throw(ArgumentError("ntimepoints does not aggree with ntimepoints"))
+        end
     end
-    if !ismissing(rating(timecoding)) && ntimepoints != implied_ntimepoints
-        throw(ArgumentError("ntimepoints does not aggree with ntimepoints"))
-    end
+    
     LGCM(timecoding, 
         ntimepoints, 
         npredictors, 
