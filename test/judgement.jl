a2c565353339b62d2dd852971b6c42ddc5e57693
Aaron Peikert <aaron.peikert@posteo.de>
2022-08-02 15:21:38 +0200

    increase test coverage

--- test/judgement.jl
+++ test/judgement.jl
@@ -2,4 +2,5 @@
     @test J(1.3) == Judgement(1.3, 1.0)
     @test J(missing) == Judgement(missing, 1.0)
     @test convert(Judgement, 2.0) == Judgement(2.0, 1.0)
+    @test_throws ArgumentError Judgement(2.0, 2.0)
 end
