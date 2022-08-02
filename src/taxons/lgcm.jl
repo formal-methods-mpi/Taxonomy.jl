f703c08ec3adadaa609fb34ea6b5f9d0803448b8
Aaron Peikert <aaron.peikert@posteo.de>
2022-08-02 15:08:51 +0200

    treat lgcm types more lax

--- src/taxons/lgcm.jl
+++ src/taxons/lgcm.jl
@@ -6,17 +6,18 @@ struct LGCM <: Taxon
 end
 
 function LGCM(; 
-    timecoding, 
-    ntimepoints =  
-    if isa(timecoding, Vector{<:Number})
-        length(timecoding)
-    elseif ismissing(timecoding)
-        missing
-    else
-        throw(DomainError(timecoding, "please provide a numeric vector or missing for timecoding argument"))
-    end,
+    timecoding = NoJudgement(), 
+    ntimepoints = NoJudgement(),
     npredictors = 0,
     nonlinearfunction = 0)
+
+    implied_ntimepoints = length(rating(timecoding))
+    if ismissing(rating(ntimepoints))
+        ntimepoints = implied_ntimepoints
+    end
+    if !ismissing(rating(timecoding)) && ntimepoints != implied_ntimepoints
+        throw(DomainError("ntimepoints does not aggree with ntimepoints"))
+    end
     LGCM(timecoding, 
         ntimepoints, 
         npredictors, 
