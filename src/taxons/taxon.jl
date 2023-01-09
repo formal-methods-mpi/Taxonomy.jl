2f8f95251864dc38cbc5631f5e4fbe41e14a1c33
leoniehagitte <108812346+LeonieHagitte@users.noreply.github.com>
2023-01-09 10:56:44 +0100

    Update taxon.jl

--- src/taxons/taxon.jl
+++ src/taxons/taxon.jl
@@ -2,7 +2,7 @@ struct NoTaxon <: Taxon end
 
 #A Taxon to show, that there is in fact a model to be coded, but this is at the current point not possible
 struct NoTaxonYet <: Taxon end
-function Record(; rater = missing, id = missing, location = missing, meta = missing, taxons = missing, spec = missing, data = missing)
+function Record(; rater = missing, id = missing, location = missing, meta = missing, taxons = NoTaxonYet, spec = missing, data = missing)
     if taxons = NoTaxonYet
         @warn "This model is currently not possible to code? - please come back later."
     end
