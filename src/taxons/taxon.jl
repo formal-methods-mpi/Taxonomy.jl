2eaf81ad25d41a55c552ed9f123b5796634a2277
leoniehagitte <108812346+LeonieHagitte@users.noreply.github.com>
2023-01-09 11:05:53 +0100

    #A Taxon to show, that there is no pathmodel to code in this paper

--- src/taxons/taxon.jl
+++ src/taxons/taxon.jl
@@ -1,3 +1,4 @@
+#A Taxon to show, that there is no pathmodel to code in this paper
 struct NoTaxon <: Taxon end
 
 #A Taxon to show, that there is in fact a model to be coded, but this is at the current point not possible
