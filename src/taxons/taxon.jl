fa24c48d9b5b2a59e7f354c38f7918ab6d2e4bba
leoniehagitte <108812346+LeonieHagitte@users.noreply.github.com>
2023-01-09 10:56:10 +0100

    first try to implement a new taxon for intentional missings
    #A Taxon to show, that there is in fact a model to be coded, but this is at the current point not possible
    struct NoTaxonYet <: Taxon end
    function Record(; rater = missing, id = missing, location = missing, meta = missing, taxons = missing, spec = missing, data = missing)
        if taxons = NoTaxonYet
            @warn "This model is currently not possible to code? - please come back later."
        end

--- src/taxons/taxon.jl
+++ src/taxons/taxon.jl
@@ -1,1 +1,8 @@
 struct NoTaxon <: Taxon end
+
+#A Taxon to show, that there is in fact a model to be coded, but this is at the current point not possible
+struct NoTaxonYet <: Taxon end
+function Record(; rater = missing, id = missing, location = missing, meta = missing, taxons = missing, spec = missing, data = missing)
+    if taxons = NoTaxonYet
+        @warn "This model is currently not possible to code? - please come back later."
+    end
