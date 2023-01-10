89192b6c142ca4675919fa6c0375aff3a2af67e1
nickhaf <hafiznij@hu-berlin.de>
2023-01-10 12:02:17 +0100

    factor_variance to float, updated doctests

--- src/taxons/cfa.jl
+++ src/taxons/cfa.jl
@@ -15,10 +15,10 @@ Building Block for CFA Taxonomy. Multiple Factors can be combined to a CFA.
 - `crossloadings_outgoing`: Vector of crossloadings going to other items which have higher loadings from other factors. 
 
 ```jldoctest
-Factor(nobserved = 2, loadings = [1, 0.4])
+Factor(nobserved = 2, loadings = [1, 0.4], factor_variance = 0.6)
 
 # output
-Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Vector{Float64}}([1.0, 0.4], 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing))
+Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Vector{Float64}}([1.0, 0.4], 1.0, missing), Judgement{Float64}(0.6, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing))
 ```
 """
 
@@ -26,7 +26,7 @@ Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Vector{Float64}}([1.0, 0.4],
 struct Factor <: AbstractCFA
      nobserved::Judgement{ <: Union{ <:Int, Missing}}
      loadings::Judgement{ <: Union{ <: AbstractArray{ <: Number}, Missing}}
-     factor_variance::Judgement{ <: Union{ <:Int, Missing}}
+     factor_variance::Judgement{ <: Union{ <:Number, Missing}}
      error_covariances_within::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
      error_covariances_between::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
      crossloadings_incoming::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
@@ -59,8 +59,8 @@ Consists of Factors (measurement model) and a graph from StenoGraphs (structural
 using StenoGraphs
 using Taxonomy
 
-factor1 = Factor(nobserved = 2, loadings = [1, 0.4])
-factor2 = Factor(nobserved = 2, loadings = [0.7, 0.3])
+factor1 = Factor(nobserved = 2, loadings = [1, 0.4], factor_variance = 0.7)
+factor2 = Factor(nobserved = 2, loadings = [0.7, 0.3], factor_variance = 1)
 
 graph = @StenoGraph begin
     # latent regressions
@@ -71,7 +71,7 @@ CFA(measurement_model = [factor1, factor2],
 structural_model = graph )
 
 # output
-CFA(Judgement{Vector{Factor}}(Factor[Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Vector{Float64}}([1.0, 0.4], 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing)), Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Vector{Float64}}([0.7, 0.3], 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing))], 1.0, missing), Judgement{Vector{DirectedEdge{SimpleNode{Symbol}, SimpleNode{Symbol}}}}(fac1 → fac2
+CFA(Judgement{Vector{Factor}}(Factor[Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Vector{Float64}}([1.0, 0.4], 1.0, missing), Judgement{Float64}(0.7, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing)), Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Vector{Float64}}([0.7, 0.3], 1.0, missing), Judgement{Int64}(1, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing))], 1.0, missing), Judgement{Vector{DirectedEdge{SimpleNode{Symbol}, SimpleNode{Symbol}}}}(fac1 → fac2
 , 1.0, missing))
 ```
 """
