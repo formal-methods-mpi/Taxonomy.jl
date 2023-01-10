7c20120871ad8239895863f535e963cb276f6dec
nickhaf <hafiznij@hu-berlin.de>
2023-01-10 11:33:32 +0100

    added tests for factor_variance

--- test/taxons/cfa.jl
+++ test/taxons/cfa.jl
@@ -1,4 +1,11 @@
 @testset "Factor" begin
-#    @test Factor(2, [1, 0.4], 0, 0, 0, 0) == Factor(nobserved = J(2), loadings = J([1, 0.4])) ## WARUUUUM?
-    @test_throws MethodError Factor(nobserved = "hi", loadings = [1, 0.4])
+
+    test_factor_1 = Factor(nobserved = J(2), loadings = J([1, 0.4]), factor_variance = J(1))
+    test_factor_2 = Factor(2, [1, 0.4], 1, 0, 0, 0, 0)
+
+    @test test_factor_1.factor_variance.rating == test_factor_2.factor_variance.rating == 1
+    @test test_factor_2.factor_variance == Judgement(1, 1.0, missing)
+    
+    @test_throws MethodError Factor(nobserved = "hi", loadings = [1, 0.4], factor_variance = 1)
+
 end
