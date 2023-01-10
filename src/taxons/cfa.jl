f6f2f35bde1e30f6e1c63abcfbe721ec4b7a53b0
nickhaf <hafiznij@hu-berlin.de>
2023-01-10 11:11:16 +0100

    added factor_variance

--- src/taxons/cfa.jl
+++ src/taxons/cfa.jl
@@ -8,6 +8,7 @@ Building Block for CFA Taxonomy. Multiple Factors can be combined to a CFA.
 
 - `nobserved`: Number of manifest variables.
 - `loadings`: Vector of loadings, one for each item. 
+- `factor_variance`: Variance of the factor.
 - `error_covariances_within`: Vector of covariances within factor.
 - `error_covariances_between`: Vector of covariances the factor shares with a different factor. 
 - `crossloadings_incoming`: Vector of crossloadings coming from other factors. They should be lower than the loading coming to the item from this factor.  
@@ -25,21 +26,23 @@ Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Vector{Float64}}([1.0, 0.4],
 struct Factor <: AbstractCFA
      nobserved::Judgement{ <: Union{ <:Int, Missing}}
      loadings::Judgement{ <: Union{ <: AbstractArray{ <: Number}, Missing}}
+     factor_variance::Judgement{ <: Union{ <:Int, Missing}}
      error_covariances_within::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
      error_covariances_between::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
      crossloadings_incoming::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
      crossloadings_outgoing::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
-    Factor(nobserved, loadings, error_covariances_within, error_covariances_between, crossloadings_incoming, crossloadings_outgoing) =
-        new(J(nobserved), J(loadings), J(error_covariances_within), J(error_covariances_between), J(crossloadings_incoming), J(crossloadings_outgoing))
+    Factor(nobserved, loadings, factor_variance, error_covariances_within, error_covariances_between, crossloadings_incoming, crossloadings_outgoing) =
+        new(J(nobserved), J(loadings), J(factor_variance), J(error_covariances_within), J(error_covariances_between), J(crossloadings_incoming), J(crossloadings_outgoing))
 end
 
 function Factor(;nobserved,
     loadings, 
+    factor_variance,
     error_covariances_within = 0,
     error_covariances_between = 0, 
     crossloadings_incoming = 0,
     crossloadings_outgoing = 0)
-    Factor(nobserved, loadings, error_covariances_within, error_covariances_between, crossloadings_incoming, crossloadings_outgoing)
+    Factor(nobserved, loadings, factor_variance, error_covariances_within, error_covariances_between, crossloadings_incoming, crossloadings_outgoing)
 end
 
 
