87552b87ae010919daf15a38c87fcc8d82b47458
nickhaf <hafiznij@hu-berlin.de>
2023-01-11 12:53:29 +0100

    remove between factor variances for standalone factor

--- src/taxons/standalone_factor.jl
+++ src/taxons/standalone_factor.jl
@@ -9,9 +9,6 @@ Taxon for Models with only one factor.
 - `n_sample`: Sample size.
 - `loadings`: Vector of loadings, one for each item. 
 - `error_covariances_within`: Vector of covariances within factor.
-- `error_covariances_between`: Vector of covariances the factor shares with a different factor. 
-- `crossloadings_incoming`: Vector of crossloadings coming from other factors. They should be lower than the loading coming to the item from this factor.  
-- `crossloadings_outgoing`: Vector of crossloadings going to other items which have higher loadings from other factors. 
 
 ```jldoctest
 Standalone_Factor(nobserved = 2, n_sample = 100, loadings = [1, 0.4])
@@ -26,19 +23,13 @@ struct Standalone_Factor <: AbstractCFA
      n_sample::Judgement{ <: Union{ <:Int, Missing}}
      loadings::Judgement{ <: Union{ <: AbstractArray{ <: Number}, Missing}}
      error_covariances_within::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
-     error_covariances_between::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
-     crossloadings_incoming::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
-     crossloadings_outgoing::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
-    Standalone_Factor(nobserved, n_sample, loadings, error_covariances_within, error_covariances_between, crossloadings_incoming, crossloadings_outgoing) =
-        new(J(nobserved), J(n_sample), J(loadings), J(error_covariances_within), J(error_covariances_between), J(crossloadings_incoming), J(crossloadings_outgoing))
+    Standalone_Factor(nobserved, n_sample, loadings, error_covariances_within) =
+        new(J(nobserved), J(n_sample), J(loadings), J(error_covariances_within))
 end
 
 function Standalone_Factor(;nobserved,
     n_sample, 
     loadings, 
     error_covariances_within = 0,
-    error_covariances_between = 0, 
-    crossloadings_incoming = 0,
-    crossloadings_outgoing = 0)
-    Standalone_Factor(nobserved, n_sample, loadings, error_covariances_within, error_covariances_between, crossloadings_incoming, crossloadings_outgoing)
+    Standalone_Factor(nobserved, n_sample, loadings, error_covariances_within)
 end
