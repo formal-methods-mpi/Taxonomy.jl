cf1397f6e22883ac9025edce3d679729db21a10a
Aaron Peikert <aaron.peikert@posteo.de>
2023-01-13 11:55:48 +0100

    standalone should be based on factor

--- src/taxons/standalone_factor.jl
+++ src/taxons/standalone_factor.jl
@@ -4,10 +4,10 @@ Taxon for Models with only one factor.
 
 ## Arguments
 
-- `n_observed`: Number of manifest variables.
-- `n_sample`: Sample size.
-- `loadings`: Vector of loadings, one for each item. 
-- `error_covariances_within`: Vector of covariances within factor.
+- `nsample`: Sample size.
+- `, kwargs...`: all other arguments are passed onto [`Factor`](@ref)
+
+All others
 
 ```jldoctest
 Standalone_Factor(n_observed = 2, n_sample = 100, loadings = [1, 0.4])
@@ -15,19 +15,6 @@ Standalone_Factor(n_observed = 2, n_sample = 100, loadings = [1, 0.4])
 # output
 Standalone_Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Int64}(100, 1.0, missing), Judgement{Vector{Float64}}([1.0, 0.4], 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing))```
 """
-
-struct Standalone_Factor <: AbstractCFA
-     n_observed::Judgement{ <: Union{ <:Int, Missing}}
-     n_sample::Judgement{ <: Union{ <:Int, Missing}}
-     loadings::Judgement{ <: Union{ <: AbstractArray{ <: Number}, Missing}}
-     error_covariances_within::Judgement{ <: Union{ <: AbstractArray{ <: Number}, <: Int, Missing}}
-    Standalone_Factor(n_observed, n_sample, loadings, error_covariances_within) =
-        new(J(n_observed), J(n_sample), J(loadings), J(error_covariances_within))
-end
-
-function Standalone_Factor(;n_observed,
-    n_sample, 
-    loadings, 
-    error_covariances_within = 0)
-    Standalone_Factor(n_observed, n_sample, loadings, error_covariances_within)
+function Standalone_Factor(;n_sample, kwargs...)
+    Factor(;n_sample, kwargs...)
 end
