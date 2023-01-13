d87ae7576150f3bb3cb54eef7f258a9073376539
Aaron Peikert <aaron.peikert@posteo.de>
2023-01-13 14:47:49 +0100

    rename according to #54

--- src/taxons/standalone_factor.jl
+++ src/taxons/standalone_factor.jl
@@ -15,6 +15,6 @@ Standalone_Factor(n_observed = 2, n_sample = 100, loadings = [1, 0.4])
 # output
 Standalone_Factor(Judgement{Int64}(2, 1.0, missing), Judgement{Int64}(100, 1.0, missing), Judgement{Vector{Float64}}([1.0, 0.4], 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing))```
 """
-function Standalone_Factor(;n_sample, kwargs...)
-    Factor(;n_sample, kwargs...)
+function Standalone_Factor(;n_observed, kwargs...)
+    Factor(;n_observed, kwargs...)
 end
