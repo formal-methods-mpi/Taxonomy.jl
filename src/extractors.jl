e13c3ae531f51f987a42e148c13b8bffc0ca7dfe
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-01-16 09:51:08 +0100

    add extractor for factor_variance

--- src/extractors.jl
+++ src/extractors.jl
@@ -20,3 +20,28 @@ rating(n_sample(f))
 function n_sample(x::Taxon)
     x.n_sample
 end
+
+
+
+"""
+Function to extract factor variance.
+
+## Arguments
+
+- `x`: Something of type [`Taxon`](@ref)
+
+## Return
+
+Returns a [`Judgement`](@ref)
+
+```jldoctest
+f = Factor(n_sample = 100, n_variables = 2, loadings = [1, 0.4], factor_variance = 1.0)
+rating(factor_variance(f))
+
+# output
+1.0
+```
+"""
+function factor_variance(x::Taxon)
+    x.factor_variance
+end
