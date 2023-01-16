82fde7098b98e21d0166f7985d1dc64ebfc32ec4
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-01-16 11:38:20 +0100

    added documentation for LGCM

--- src/taxons/lgcm.jl
+++ src/taxons/lgcm.jl
@@ -1,3 +1,22 @@
+"""
+LGCM Taxon. 
+Taxon for Linear Growth Curve Model.
+
+    ## Arguments
+
+- `n_sample`: Number of observed cases.
+- `timecoding`: Vector containing the coding of the measurement time points (loadings of the slope onto the items).
+- `n_timepoints`: Number of measurement time points. Should normally be the length of timecoding.  
+- `n_predictors`: Number of predictors for intercept and/or slope. 
+- `non_linear_function`: If a nonlinear function is included, the respective slopes can be added here. 
+
+```jldoctest
+Factor(n_variables = 2, loadings = [1, 0.4])
+
+# output
+Factor(Judgement{Missing}(missing, 1.0, missing), Judgement{Int64}(2, 1.0, missing), Judgement{Vector{Float64}}([1.0, 0.4], 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing), Judgement{Int64}(0, 1.0, missing))
+```
+"""
 struct LGCM <: Taxon
     n_sample::Judgement{ <: Union{ <:Int, Missing}}
     timecoding::Judgement{ <: Union{ <: AbstractArray{ <: Number}, Missing}}
