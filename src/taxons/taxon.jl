e457dc643ae55a100882407095fe6a3567458204
leoniehagitte <108812346+LeonieHagitte@users.noreply.github.com>
2023-01-20 09:23:56 +0100

    change destination of function
    function Record(; rater = missing, id = missing, location = missing, meta = missing, taxons = NoTaxonYet, spec = missing, data = missing)
        if taxons = NoTaxonYet
            @warn "This model is currently not possible to code? - please come back later."
        end

--- src/taxons/taxon.jl
+++ src/taxons/taxon.jl
@@ -3,7 +3,4 @@ struct NoTaxon <: Taxon end
 
 #A Taxon to show, that there is in fact a model to be coded, but this is at the current point not possible
 struct NoTaxonYet <: Taxon end
-function Record(; rater = missing, id = missing, location = missing, meta = missing, taxons = NoTaxonYet, spec = missing, data = missing)
-    if taxons = NoTaxonYet
-        @warn "This model is currently not possible to code? - please come back later."
-    end
+
