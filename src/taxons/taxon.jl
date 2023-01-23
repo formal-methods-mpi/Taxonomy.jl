da3085aea4208bc9c8d45d1aa6aeb83a1b5f3562
leoniehagitte <108812346+LeonieHagitte@users.noreply.github.com>
2023-01-23 09:13:48 +0100

    description for the access date option
    but this is more a placeholder right now

--- src/taxons/taxon.jl
+++ src/taxons/taxon.jl
@@ -3,6 +3,7 @@ No taxons Taxon.
 - `NoTaxon` : A Taxon to show, that there is no pathmodel to code in this paper
 - `NoTaxonYet`:  A Taxon to show, that there is in fact a model to be coded, but this is at the current point not possible
       NoTaxonYet gives you the option to name the 'modeltype', you were not able to code, to spare your future self the work of going everything through.
+      NoTaxonYet is also supposed to give your Record a timestamp 'accessdate' to further specify at what point there were no possibility to code the respective model.
 
 ```jldoctest
 
