39ffd5c5107fd284582d02bc4258b001ca92bc80
leoniehagitte <108812346+LeonieHagitte@users.noreply.github.com>
2023-01-23 08:58:20 +0100

    added description for the model type string

--- src/taxons/taxon.jl
+++ src/taxons/taxon.jl
@@ -2,6 +2,7 @@
 No taxons Taxon. 
 - `NoTaxon` : A Taxon to show, that there is no pathmodel to code in this paper
 - `NoTaxonYet`:  A Taxon to show, that there is in fact a model to be coded, but this is at the current point not possible
+      NoTaxonYet gives you the option to name the 'modeltype', you were not able to code, to spare your future self the work of going everything through.
 
 ```jldoctest
 
