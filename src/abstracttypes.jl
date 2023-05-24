2388e5546c60ee14a51a3bc29085395c1ea004c6
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-05-24 14:33:50 +0200

    first model hierarchy layout

--- src/abstracttypes.jl
+++ src/abstracttypes.jl
@@ -5,5 +5,25 @@ abstract type AbstractMeta end
 Taxon is the supertype of all taxons.
 """
 abstract type Taxon end
-abstract type Pathmodel <: Taxon end
-abstract type AbstractFactor <: Taxon end
+
+abstract type AbstractPathmodel <: Taxon end
+abstract type AbstractCFA <: Taxon end
+abstract type LGCM <: Taxon end
+abstract type CLPM <: Taxon end
+
+struct Measurement <: AbstractCFA end
+
+struct Structural <: AbstractPathmodel end
+    
+struct LatentPathmodel <: AbstractPathmodel 
+    structural::Structural
+    measurement::Measurement
+end
+
+struct BifactorCFA <: AbstractCFA 
+    measurement::HierarchicalCFA
+end
+
+struct HierarchicalCFA <: AbstractCFA
+    measurement::Measurement
+end
