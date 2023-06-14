d00bbf31f8343d39fc699b53c3d6d2b80ec246f5
Leonie H <leonie.hagitte@student.hu-berlin.de>
2023-06-14 16:30:30 +0200

    measurement delete

--- src/taxons/structural.jl
+++ src/taxons/structural.jl
@@ -6,7 +6,6 @@ Consists of a graph from StenoGraphs structural (structural model) and a measure
 ## Arguments
 
 - `structural_model`: Graph from StenoGraphs package. Defines the latent relations between the factors of measurement_model.  
-- `measurement_model`: Vector of Factors.
 
 ```julia
 using StenoGraphs
@@ -20,29 +19,25 @@ graph = @StenoGraph begin
     fac1 → fac2
 end
 
-Structural(measurement_model = [factor1, factor2], 
-structural_model = graph)
+Structural(structural_model = graph)
 
 # output
 
 Sructural
    n_sample: Judgement{Missing}
-   measurement_model: Judgement{Vector{Measurement}}
    structural_model: Judgement{Vector{DirectedEdge{SimpleNode{Symbol}, SimpleNode{Symbol}}}}
 
 ```
 """
 struct Structural <: AbstractPathmodel
     n_sample::Judgement{<: Int}
-    measurement_model::Judgement{ <: Union{<:AbstractArray{<: AbstractCFA}, Missing}}
     structural_model::Judgement{ <: Union{<:AbstractArray{<: StenoGraphs.AbstractEdge}, Missing}}
-    Structural(n_sample, measurement_model, structural_model) = new(J(n_sample), J(measurement_model), J(structural_model))
+    Structural(n_sample, structural_model) = new(J(n_sample), J(structural_model))
 end
 
 function Structural(;n_sample = missing,
-    measurement_model,
     structural_model) # hier basic CFA model festlegen
-    Structural(n_sample, measurement_model, structural_model)
+    Structural(n_sample, structural_model)
 end
 
 
