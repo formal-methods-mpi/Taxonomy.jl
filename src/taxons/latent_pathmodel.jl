0c5a960c6f98eaf2c18c84dd449e1ea2182fa211
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-06-21 12:48:08 +0200

    doctests for LatentPathmodel

--- src/taxons/latent_pathmodel.jl
+++ src/taxons/latent_pathmodel.jl
@@ -1,5 +1,43 @@
 """
-add dokumentation here!
+Taxon for Linear Growth Curve Model.
+
+LatentPathmodel(;
+n_sample = missing, 
+structural_model,
+n_variables,
+loadings, 
+factor_variance,
+error_variances = 0,
+error_covariances_within = 0,
+error_covariances_between = 0, 
+crossloadings_incoming = 0,
+crossloadings_outgoing = 0
+)
+
+Create a new `LatentPathmodel` instance.
+
+# Examples
+
+```jldoctest
+julia> structural_model_example = Structural(...) # replace with actual example
+julia> measurement_model_example = Measurement(...) # replace with actual example
+
+julia> model = LatentPathmodel(
+n_sample = 100, 
+structural_model = structural_model_example,
+n_variables = 5,
+loadings = [0.7, 0.6, 0.8, 0.5, 0.9], 
+factor_variance = 1.0,
+error_variances = 0.05,
+error_covariances_within = 0,
+error_covariances_between = 0, 
+crossloadings_incoming = 0,
+crossloadings_outgoing = 0
+)
+
+# output
+
+LatentPathmodel(structural_model_example, measurement_model_example)
 """
 struct LatentPathmodel <: AbstractPathmodel 
     structural_model::Structural
