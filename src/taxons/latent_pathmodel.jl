dfa6c8a727c23ef5a1a3a700ae52bb9cdfdb18e8
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-06-21 11:48:45 +0200

    allow for Measurement and Structrual arguments in LatentPathmodel

--- src/taxons/latent_pathmodel.jl
+++ src/taxons/latent_pathmodel.jl
@@ -7,7 +7,28 @@ struct LatentPathmodel <: AbstractPathmodel
 end
 
 function LatentPathmodel(;
-    structural_model,
-    measurement_model)
-    LatentPathmodel(structural_model, measurement_model)
+        n_sample = missing, 
+        structural_model,
+        n_variables,
+        loadings, 
+        factor_variance,
+        error_variances = 0,
+        error_covariances_within = 0,
+        error_covariances_between = 0, 
+        crossloadings_incoming = 0,
+        crossloadings_outgoing = 0
+    )
+    structural = Structural(n_sample = n_sample, structural_model = structural_model)
+    measurement = Measurement(
+        n_sample = n_sample, 
+        n_variables = n_variables,
+        loadings = loadings, 
+        factor_variance = factor_variance,
+        error_variances = error_variances,
+        error_covariances_within = error_covariances_within,
+        error_covariances_between = error_covariances_between,
+        crossloadings_incoming = crossloadings_incoming,
+        crossloadings_outgoing = crossloadings_outgoing
+        )
+    LatentPathmodel(structural, measurement)
 end
