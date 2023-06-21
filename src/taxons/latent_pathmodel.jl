6cd8c4aac375647d13cd3abf890d545fed4fccbd
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-06-21 14:00:12 +0200

    try fix function

--- src/taxons/latent_pathmodel.jl
+++ src/taxons/latent_pathmodel.jl
@@ -50,18 +50,43 @@ struct LatentPathmodel <: AbstractPathmodel
     measurement_model::Measurement
 end
 
+
 function LatentPathmodel(;
-    structural = Structural(n_sample, structural_model),
+    n_sample = missing, 
+    structural_model,
+    n_variables,
+    loadings, 
+    factor_variance,
+    error_variances = 0,
+    error_covariances_within = 0,
+    error_covariances_between = 0, 
+    crossloadings_incoming = 0,
+    crossloadings_outgoing = 0
+    )
+    structural = Structural(n_sample = n_sample, structural_model = structural_model)
     measurement = Measurement(
-        n_sample = missing,
-        n_variables,
-        loadings, 
-        factor_variance,
-        error_variances = 0,
-        error_covariances_within = 0,
-        error_covariances_between = 0, 
-        crossloadings_incoming = 0,
-        crossloadings_outgoing = 0
-        ))
+    n_sample = n_sample, 
+    n_variables = n_variables,
+    loadings = loadings, 
+    factor_variance = factor_variance,
+    error_variances = error_variances,
+    error_covariances_within = error_covariances_within,
+    error_covariances_between = error_covariances_between,
+    crossloadings_incoming = crossloadings_incoming,
+    crossloadings_outgoing = crossloadings_outgoing
+    )
     LatentPathmodel(structural, measurement)
 end
+
+
+using Taxonomy
+using StenoGraphs
+
+graph = @StenoGraph begin
+    # latent regressions
+    fac1 → fac2
+end
+
+LatentPathmodel(
+    Structural(n_sample = 12, structural_model = graph),
+    Measurement(n_variables = 2, loadings = [1, 0.4], factor_variance = 0.6))
