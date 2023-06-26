89e513007c6d5ffc026ac75ac8d4222c0da677bc
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-06-26 13:03:11 +0200

    add extractor for structural_model

--- src/extractors.jl
+++ src/extractors.jl
@@ -43,3 +43,35 @@ rating(factor_variance(f))
 function factor_variance(x::Measurement)
     x.factor_variance
 end
+
+
+"""
+Function to extract the `StenoGraphs` structural model from [`Structural`].
+
+## Arguments
+
+- `x`: [`Strucutral`](@ref).
+
+## Return
+
+Returns a [`Judgement`](@ref)
+
+```julia-repl
+using Taxonomy
+using StenoGraphs
+
+graph = @StenoGraph begin
+    # latent regressions
+    fac1 → fac2
+end
+
+struct_model = Structural(structural_model = graph)
+
+structural_model(struct_model)
+```
+"""
+function structural_model(x::Structural)
+    x.structural_model
+end
+
+
---
+++ test/taxons/structural.jl
@@ -0,0 +1,12 @@
+@testset "Structural" begin
+    using StenoGraphs
+
+    graph = @StenoGraph begin
+        # latent regressions
+        fac1 → fac2
+    end
+
+    struct_model = Structural(structural_model = graph)
+
+    @test rating(structural_model(struct_model)) == graph
+end
