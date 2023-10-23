4f6d82ea982f8a6f42b281f39d429f6914ec3277
Nicklas Hafiz <nicklashaf@gmail.com>
2023-10-23 16:31:43 +0200

    study does something now

--- src/judgements/study.jl
+++ src/judgements/study.jl
@@ -3,18 +3,28 @@
 
 Stores a dictionary with StudyJudgements.
 """
-struct Study <: AbstractJudgement{Dict} end
+
+## This needs to get an abstract type? Same as Record, model
+
+struct Study <: JudgementLevel
+    judgements::Union{Dict{Symbol, Vector{AbstractJudgement}}, Missing}
+end
+
+function Study(j::AbstractJudgement...)
+    check_judgement_level.(j, (StudyJudgement(), ))
+    judgements = judgement_dict(j...)
+    Study(judgements)
+end
+
+
+# Next action: 
+## Add warning in judgement_dict if the same key appears twice
 
 #function Study(j::AbstractJudgement...)
 #    check_judgement_level.(j, (StudyJudgement(), ))
 #    Study()
 #end
 
-function Study(x...)
-    @warn("`Study()` is just a mock and doesnt store anything.")
-    Study()
-end
-
-judgement_level(x::Study) = RecordJudgement()
+judgement_level(x::Study) = StudyJudgement()
 judgement_unique(x::Study) = true
 judgement_key(x::Study) = :Study
