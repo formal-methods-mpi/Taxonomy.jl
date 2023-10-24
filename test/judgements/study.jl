fcf4a211a78c8a6014c586286ef77987a5fb0813
Nicklas Hafiz <nicklashaf@gmail.com>
2023-10-24 14:32:07 +0200

    simple Test for Study()

--- test/judgements/study.jl
+++ test/judgements/study.jl
@@ -1,11 +1,15 @@
-using Taxonomy
-
+using Taxonomy, Test
 include("../../src/judgements/Judgements.jl")
 using .Judgements
 
 
-@testset "" begin
-
-    simple_study_test = Study(N(1000, 1))
-
+@testset "Simple Study example provides the expected output" begin
+    simple_study_test_judgements = judgements(Study(N(1000, 1), Empirical(true, 1)))
+    @test typeof(simple_study_test_judgements[:N][1]) == N{Int64}
+    @test typeof(simple_study_test_judgements[:Empirical][1]) == Empirical{Bool}
 end
+
+@testset "Study() can not take Judgements of RecordLevel typpe" begin
+    @test_throws ArgumentError Study(Lang("de", 1))
+end
+
