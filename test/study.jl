df07635810b1e1d0f6279d5fb3ff0a6d7a8f47d1
Nicklas Hafiz <nicklashaf@gmail.com>
2023-10-31 10:11:21 +0100

    Add rating test for Study()

--- test/study.jl
+++ test/study.jl
@@ -1,7 +1,9 @@
 @testset "Simple Study example provides the expected output" begin
     simple_study_test_judgements = judgements(Study(N(1000, 1), Empirical(true, 1)))
+    
     @test typeof(simple_study_test_judgements[:N][1]) == N{Int64}
     @test typeof(simple_study_test_judgements[:Empirical][1]) == Empirical{Bool}
+    @test rating(simple_study_test_judgements[:Empirical][1]) == true
 end
 
 @testset "Study() can not take Judgements of RecordLevel typpe" begin
