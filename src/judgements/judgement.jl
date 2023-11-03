63e9f3fa4f7ed7ef475767e7ab0402c772723a16
Nicklas Hafiz <nicklashaf@gmail.com>
2023-11-03 16:00:02 +0100

    format judgement

--- src/judgements/judgement.jl
+++ src/judgements/judgement.jl
@@ -19,33 +19,38 @@ For which level this judgement is applicable.
 """
 function judgement_level end
 
-function correct_judgement_level(x, level = AnyLevelJudgement)
+function correct_judgement_level(x, level=AnyLevelJudgement)
     x in (level, AnyLevelJudgement)
 end
 
-function check_judgement_level(x, level = AnyLevelJudgement)
+function check_judgement_level(x::AbstractJudgement, level=AnyLevelJudgement)
+
     x = x isa JudgementLevel ? x : judgement_level(x)
     if !correct_judgement_level(x, level)
-        throw(ArgumentError("An `$level` was requires but an `$x` was given."))
+        throw(ArgumentError("A `$level` was required but a `$x` was given."))
     end
     nothing
 end
 
-macro newjudgement(name, level, doc, type = Any, check = x -> nothing, unique = true)
+function check_judgement_level(x, level=AnyLevelJudgement)
+    return (x)
+end
+
+macro newjudgement(name, level, doc, type=Any, check=x -> nothing, unique=true)
     inner = quote
-            struct $name{T <: Union{<: $(type), Missing}} <: AbstractJudgement{T}
+        struct $name{T<:Union{<:$(type),Missing}} <: AbstractJudgement{T}
             rating::T
             certainty::Float64
-            comment::Union{String, Missing}
-            function $name{T}(r, c = 1.0, l = missing) where T
+            comment::Union{String,Missing}
+            function $name{T}(r, c=1.0, l=missing) where {T}
                 $check(r)
                 Judgements.check_certainty(c)
                 new{T}(r, c, l)
             end
         end
     end
-    outer = :($name(r::T, c = 1.0, l = missing) where T = $name{T}(r, c, l))
-    outer2 = :($name(r::AbstractJudgement, c = 1.0, l = missing) = throw(ArgumentError("Judgements of Judgements are too meta.")))
+    outer = :($name(r::T, c=1.0, l=missing) where {T} = $name{T}(r, c, l))
+    outer2 = :($name(r::AbstractJudgement, c=1.0, l=missing) = throw(ArgumentError("Judgements of Judgements are too meta.")))
     doc = :(@doc """
         $($name)(r::Union{<: $($type), Missing}, c = 1.0, l = missing})
 
@@ -75,13 +80,13 @@ end
     AnyLevelJudgement,
     """
     A generic judgment without any checks on content.
-    
+
     ## Arguments
-    
+
     - `rating`: The rating, e.g. "Structural" or 1.0.
     - `certainty`: If uncertain, a number between 0.0 and 1.0 (0-100%)
     - `comment`: information on why the judgement was made, may contain information about the source within the paper, e.g., section, page, table number, figure number.
-    
+
     ```jldoctest
     julia> Judgement(1.0, .99, "Figure 1");
     ```
@@ -112,16 +117,16 @@ Shorthand for [`Judgement`](@ref)
 """
 const J = Judgement
 
-Base.convert(::Type{T}, x) where {T <: AbstractJudgement} = T(x)
-function Base.convert(::Type{T}, x::AbstractJudgement) where {T <: AbstractJudgement} 
+Base.convert(::Type{T}, x) where {T<:AbstractJudgement} = T(x)
+function Base.convert(::Type{T}, x::AbstractJudgement) where {T<:AbstractJudgement}
     x isa T ? x : T(rating(x), certainty(x), comment(x))
 end
-Base.promote_rule(::Type{T1}, ::Type{T2}) where {T1 <: AbstractJudgement{T2}} where T2 = T1
+Base.promote_rule(::Type{T1}, ::Type{T2}) where {T1<:AbstractJudgement{T2}} where {T2} = T1
 
 function ==(x::AbstractJudgement, y::AbstractJudgement)
     isequal(rating(x), rating(y)) &&
-    isequal(certainty(x), certainty(y)) &&
-    isequal(comment(x), comment(y))
+        isequal(certainty(x), certainty(y)) &&
+        isequal(comment(x), comment(y))
 end
 
 """
@@ -134,4 +139,4 @@ julia> NoJudgement()
 Judgement{Missing}(missing, 0.0, missing)
 ```
 """
-NoJudgement(comment = missing) = Judgement(missing, 0.0, comment)
+NoJudgement(comment=missing) = Judgement(missing, 0.0, comment)
