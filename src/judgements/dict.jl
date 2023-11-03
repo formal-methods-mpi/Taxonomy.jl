56dcd5c2bc795e400d47c3075e6a6fd666e2b9e8
Nicklas Hafiz <nicklashaf@gmail.com>
2023-11-03 15:50:35 +0100

    dict got methods for t <: JudgementLevel

--- src/judgements/dict.jl
+++ src/judgements/dict.jl
@@ -1,7 +1,9 @@
+## We need to be able to pair judgements with its key:
 export Pair
-Base.Pair(j::AbstractJudgement) = Pair(judgement_key(j), [j])
-export Judgements
-function add_judgement!(dict, j::T, unique) where {T <: AbstractJudgement}
+Base.Pair(j::AbstractJudgement) = Pair(judgement_key(j), [j]) 
+
+## 
+function add_judgement!(dict, j::T, unique) where {T <: AbstractJudgement} 
     if unique
         push!(dict, Pair(j))
     else
@@ -10,6 +12,23 @@ function add_judgement!(dict, j::T, unique) where {T <: AbstractJudgement}
         push!(dict, judgement_key(j) => vec)
     end
 end
-add_judgement!(dict, j) = add_judgement!(dict, j, Judgements.judgement_unique(j))
-export judgement_dict
-judgement_dict(x::AbstractJudgement...) = foldl(add_judgement!, x, init = Dict{Symbol, Vector{AbstractJudgement}}())
+
+function add_judgement!(dict, j::T) where {T <: AbstractJudgement}
+    add_judgement!(dict, j, Judgements.judgement_unique(j))
+end
+
+function add_judgement!(dict, j::T) where {T <: JudgementLevel}
+    keyname = Symbol(string(typeof(j)))
+    if (keyname in keys(dict))
+        keyname = Symbol(string(keyname) * "_2") ## figure out how to give concisive names
+    end
+
+    vec = get(dict, string(typeof(j)), T[])
+    push!(vec, j)
+    push!(dict, keyname => vec)
+end
+
+function judgement_dict(x::Union{AbstractJudgement, JudgementLevel}...) 
+    foldl(add_judgement!, x, init = Dict{Symbol, Vector{Union{AbstractJudgement, JudgementLevel}}}())
+end
+
