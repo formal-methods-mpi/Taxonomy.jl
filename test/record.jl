feec77fd0916d6e8c31936b6c3e54169c052978e
Nicklas Hafiz <nicklashaf@gmail.com>
2023-11-03 15:58:13 +0100

    Testing record with Study and newjudgment

--- test/record.jl
+++ test/record.jl
@@ -1,8 +1,4 @@
-@testset "record with different Judgement types" begin
-    # Create a new RecordDatabase instance
-    my_database = RecordDatabase()
-
-    # Test:
+@testset "record only with Judgements on the record level" begin
     test_record = Record(
         rater = "AP",
         id = "823ddb98-f880-5691-aecc-d74b67fbe263",
@@ -11,5 +7,26 @@
         )
 
     @test rating(rating(judgements(test_record)[:Lang][1])) == "de"
+end
 
+
+@testset "record with Study() nested inside" begin
+# Test:
+    record_test = Record(
+        rater = "AP",
+        id = "823ddb98-f880-5691-aecc-d74b67fbe263",
+        location = DOI("10.1007/s10869-019-09648-5"),
+        Lang( #This is the Judgement we defined earlier. Within it, we can input the value, the certainty and a comments like before: 
+            "de",
+            0.5, # not clear what language the paper has
+            "abstract in both de and en", # because the abstract exists in both english and german
+        ), 
+        Study(
+            N(100, 0.8, "the abstract says 120 but table 1 is saying 100"),
+            Measurement(n_variables = 2, loadings = [0.53, 0.95], factor_variance = 0.16), # they fitted two models to the same dataset
+            Measurement(n_variables = 2, loadings = [1, 1], factor_variance = missing)
+        )
+    )
+
+    @test judgements(record_test)[:Study][1] isa Study
 end
