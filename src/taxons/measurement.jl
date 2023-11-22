0ecb1f0ab825bf223ee88106efa1ed6de55c62c0
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-11-22 16:06:58 +0100

    documentation and doctest for the Fixed struct

--- src/taxons/measurement.jl
+++ src/taxons/measurement.jl
@@ -75,13 +75,39 @@ function StandaloneFactor(; kwargs...)
     Measurement(; kwargs...)
 end
 
-
 """
-Fixed loading or variance.
+Fixed{T <: Number}
 
-To specify which parameter of measurement is Fixed
+A wrapper type for fixed parameters in a measurement model. The `Fixed` type is used to indicate that a certain numeric parameter, such as a factor variance or loading in a `Measurement`, is fixed and should not be altered during the model fitting process. 
+
+# Arguments
+- `x::T`: The numeric value being wrapped as fixed.
+
+# Usage
+Use to indicate fixed parameters in a measurement model.
+To create a fixed parameter, simply wrap the numeric value with `Fixed`. For example, `Fixed(1.0)` creates a fixed value of `1.0`.
+The strip_fixed function is provided to extract the numeric value from a Fixed object. It can also handle regular numeric types, returning the value as is.
+
+```jldoctest
+fixed_loading = Fixed(0.7)
+
+# output
+
+Fixed{Float64}(0.7)
+
+strip_fixed(fixed_loading)
+
+# output
+
+0.7
+
+strip_fixed(2.5)  # Works with regular numbers as well
+
+# output
+
+2.5
+```
 """
-
 struct Fixed{T <: Number} <: Number
     x::T
 end
