820ada0131f3577b2e35beaf2c7540c6ee386774
lkosanke <kosankle@student.hu-berlin.de>
2023-12-01 13:11:36 +0100

    added unit test

--- test/taxons/measurement.jl
+++ test/taxons/measurement.jl
@@ -9,3 +9,18 @@
     @test_throws TypeError Measurement(n_variables="hi", loadings=[1, 0.4], factor_variance=1)
 end
 
+@testset "Fixed Struct Tests" begin
+    # Test instantiation of Fixed
+    fixed_value = Fixed(0.7)
+    @test fixed_value isa Fixed
+
+    # Test the type of the wrapped value
+    @test typeof(fixed_value.x) == Float64
+
+    # Test the strip_fixed function with Fixed object
+    @test strip_fixed(fixed_value) == 0.7
+
+    # Test the strip_fixed function with a regular number
+    regular_number = 2.5
+    @test strip_fixed(regular_number) == 2.5
+end
