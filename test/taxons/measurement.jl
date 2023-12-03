d23be4ee9300799749f7ebf0265a33c6b405fb8c
Aaron Peikert <aaron.peikert@posteo.de>
2023-12-03 12:44:54 +0100

    extend tests

--- test/taxons/measurement.jl
+++ test/taxons/measurement.jl
@@ -15,7 +15,7 @@ end
     @test fixed_value isa Fixed
 
     # Test the type of the wrapped value
-    @test typeof(fixed_value.x) == Float64
+    @test typeof(strip_fixed(fixed_value)) == Float64
 
     # Test the strip_fixed function with Fixed object
     @test strip_fixed(fixed_value) == 0.7
@@ -23,4 +23,12 @@ end
     # Test the strip_fixed function with a regular number
     regular_number = 2.5
     @test strip_fixed(regular_number) == 2.5
+
+    test_factor_1 = Measurement(
+        n_variables=J(2),
+        loadings=J([1, 0.4]),
+        factor_variance=J(1)
+    )
+    @test test_factor_1 isa Measurement
+
 end
