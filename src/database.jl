d20e6b15b4be79b6e73d68c674d2ff1a76879413
Nicklas Hafiz <nicklashaf@gmail.com>
2023-12-07 16:51:11 +0100

    multiple dispatch on check_uuid

--- src/database.jl
+++ src/database.jl
@@ -55,7 +55,7 @@ Base.convert(::Type{Pair{UUID,Record}}, r::Record) = invariant_pair(r)
 # builds function to be able to push new records to the database
 # push! as function with two arguments: db of type RecordDatabase and record of type Record, returns an instance of RecordDatabase
 function Base.push!(x::RecordDatabase, new::Pair)
-    check_id(x, new)
+  #  check_id(x, new)
     push!(x.records, invariant_pair(new))
     x
 end
@@ -68,11 +68,22 @@ function Base.delete!(rd::RecordDatabase{K,V}, key::K) where {K,V}
     delete!(rd.records, key)
     rd
 end
-Base.merge(x::RecordDatabase, y::RecordDatabase) =
-    RecordDatabase(merge(x.records, y.records))
-Base.:+(x::RecordDatabase, y::RecordDatabase) = merge(x, y)
-Base.:+(x::RecordDatabase, y::Record) = push!(x, y)
-Base.:+(x::Record, y::RecordDatabase) = y + x
+function Base.merge(x::RecordDatabase, y::RecordDatabase)
+    check_uuid(x, y) 
+    return RecordDatabase(merge(x.records, y.records))
+end
+function Base.:+(x::RecordDatabase, y::RecordDatabase) 
+    check_uuid(x, y)  
+    return merge(x, y)
+end
+function Base.:+(x::RecordDatabase, y::Record) 
+    check_uuid(x, y) 
+    return push!(x, y)
+end
+function Base.:+(x::Record, y::RecordDatabase) 
+    check_uuid(x, y) 
+    return y + x
+end
 
 
 # Base.setindex!(rd::RecordDatabase{K, V}, value::K, key::V) where {K, V} = push!(rd, key => value)
@@ -84,23 +95,57 @@ Base.:+(x::Record, y::RecordDatabase) = y + x
 #     rd
 # end
 
-function check_id(x::RecordDatabase, y::Pair)
-    id_record = first(y)
-    record = get(x.records, id_record, nothing)
-    doi_record = y[2].location.doi
+## Check out the :+ definitions. Currently the checks only happen for push!, but the should probably happen for all instances. How to incorporate that cleverly?
+#function check_id(x::Union{RecordDatabase, Record}, y::Union{RecordDatabase, Record}) ## better to take a record here?
+    # id_record = first(y)
+    # record = get(x.records, id_record, nothing)
 
-    if Base.haskey(x, id_record)
-        throw(ArgumentError("The ID $id_record is already in the data base."))
+    
+    # dois = []
+    # for i in keys(x)
+    #     push!(dois, location(get(x.records, i, false)).doi)
+    # end
+
+    # if doi_record in dois 
+    #   throw(ArgumentError("The DOI $doi_record is already in the data base."))
+    # end
+
+#end
+
+export check_uuid
+function check_uuid(x::RecordDatabase, y::Record) 
+    id_y = id(y)
+    if Base.haskey(x, id_y) 
+        id_y_string = string(id_y)
+        throw(ArgumentError("The ID $id_y_string is already in the data base."))
     end
-
-    dois = []
-    for i in keys(x)
-        push!(dois, location(get(x.records, i, false)).doi)
+end
+function check_uuid(x::Record, y::RecordDatabase) 
+    id_x = id(x)
+    if Base.haskey(y, id_x) 
+        id_x_string = string(id_x)
+        throw(ArgumentError("The ID $id_x_string is already in the data base."))
     end
+end
+function check_uuid(x::RecordDatabase, y::RecordDatabase)
+    
+    keys_x = Set(keys(x))
+    keys_y = Set(keys(y))
 
-    if doi_record in dois 
-      throw(ArgumentError("The DOI $doi_record is already in the data base."))
+    duplicated_keys = intersect(keys_x, keys_y)
+
+    if length(duplicated_keys) > 0
+        duplicated_keys_str = join(duplicated_keys)
+        throw(ArgumentError("Duplicated ID(s): $duplicated_keys_str"))
     end
 
 end
 
+
+
+
+#get_doi_record = function(x::Record)
+#    doi_record = x[2].location.doi
+#    return doi_record
+#end
+    
--- test/database.jl
+++ test/database.jl
@@ -37,7 +37,36 @@
     my_database[id(second_record)] = second_record
     @test my_database[id(second_record)] == second_record
     @test_throws ArgumentError my_database[id(first_record)] = second_record
-    @test_throws ArgumentError push!(my_database, first_record)
-    @test_throws ArgumentError push!(my_database, third_record)
+
+end
+
+
+@testset "check uuids" begin
+
+    my_database = RecordDatabase()
+
+    # Test:
+    first_record = Record(
+        id="8f1713c9-482b-58cb-8ed4-128c03e9dafb",
+        rater="AP",
+        location=DOI("10.1080/10826084.2020.1735440"),
+        Lang("de")
+    )
+
+    second_record = Record(
+        id=generate_id(),
+        rater="AP",
+        location=DOI("10.2307/2095172"),
+        Lang("en")
+    )
+
+    push!(my_database, first_record)
+
+
+    id_y = id(first_record)
+    Base.haskey(my_database, id_y)
+
+    @test_throws ArgumentError check_uuid(my_database, first_record)
+    @test_throws ArgumentError check_uuid(my_database, my_database)
 
 end
