5b59bdc79e477772255d2ab8419c02e3ccce6a8e
Aaron Peikert <peikert@mpib-berlin.mpg.de>
2024-02-12 16:04:48 +0100

    fix tutorials closes #131

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,8 @@ makedocs(;
     pages=[
         "Home" => "index.md",
         "Tutorials" => ["Getting Started" => "tutorials/start.md",
-                        "Something is missing" => "tutorials/missing.md"]
+                        "Something is missing" => "tutorials/missing.md",
+                        "Installing, Updating, and Versioning Taxonomy.jl" => software-versions.md]
     ],
     doctest = true, # replace true with :fix to fix doctest
 )
