d25cd4c4686cbb392a923861bc88ca6bad6832e0
Nicklas Hafiz <nicklashaf@gmail.com>
2024-04-09 15:19:06 +0200

    filter down to Taxon level

--- src/filter.jl
+++ src/filter.jl
@@ -9,8 +9,8 @@
 #     return res_vec
 # end
 
-function Base.filter(f, s::Vector{Union{T,AbstractJudgement}} where T <: JudgementLevel)
-    res_vec = []
+function Base.filter(f, s::Vector{Union{T,AbstractJudgement}} where {T<:JudgementLevel})
+    res_vec = eltype(s)[]
     for i in eachindex(s)
         if f(s[i])
             res_vec = push!(res_vec, s[i])
@@ -20,31 +20,52 @@ function Base.filter(f, s::Vector{Union{T,AbstractJudgement}} where T <: Judgeme
     return res_vec
 end
 
+Base.filter(f, s::JudgementLevel)::Vector{Union{JudgementLevel,AbstractJudgement}} = f(s) ? [s] : []
 
-Base.filter(f, s::JudgementLevel)::Vector{Union{JudgementLevel, AbstractJudgement}} = f(s) ? [s] : []
+struct filter_levels 
+end
 
-function Base.filter(f, db::RecordDatabase, level::JudgementLevel)::RecordDatabase
 
-    if typeof(level) == Record       
+
+function Base.filter(f, db::RecordDatabase, level::AbstractString)::RecordDatabase
+
+    @assert level in ["Record", "Study", "Model", "Taxon"] "Invalid level. Choose from 'Record', 'Study', 'Model', 'Taxon'"
+
+    if typeof(level) == "Record"
         db = filter(f, db)
     end
 
-    for record in keys(db)
+    for current_record in keys(db)
 
-        record_studies = Study(db[record])
+        studies = Study(db[current_record])
 
-     
-        if typeof(level) == Study
-        judgements(db[record])[:Study] = filter(f, record_studies)
-        end
 
-        if typeof(level) == Model
-            for j in eachindex(record_studies)
-                judgements(judgements(db[record])[:Study][j])[:Model] = filter(f, Model(record_studies[j]))
+        if typeof(level) == "Study"
+            judgements(db[current_record])[:Study] = filter(f, studies)
+
+        else
+
+            for current_study in eachindex(studies)
+                models = Model(studies[current_study])
+                if typeof(level) == "Model"
+                    judgements(judgements(db[current_record])[:Study][current_study])[:Model] = filter(f, models)
+
+                else
+                    for current_model in eachindex(models)
+                        taxons = Taxon(models[current_model])
+                        if typeof(level) == "Taxon"
+                            judgements(Models(Study(db[current_record])[current_study])[current_model])[:Taxon] = filter(f, taxons)
+                        end
+                    end
+                end
             end
-        end
 
+        end
     end
-return db
+    return db
 
 end
+
+
+
+#                judgements(judgements(r[i])[:Study][j])[:Model] = length(filtered_model) > 0 ? filtered_model : Model(Study(r[i])[j])
